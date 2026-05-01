24a716783d00708c443af2a4725bdee61f01ec1d
Yoav Gershman <37370341+yoav74@users.noreply.github.com>
2026-05-01 19:24:27 +0300

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        counter = 0
+        start = 0
+        end = len(nums) - 1
+        while (start <= end):
+            counter = counter + 1
+            print(start,end)
+            index = math.floor(start + (end - start) / 2)
+            tempNum = nums[index]
+            if nums[index] == target:
+                return index
+            elif nums[index] > target:
+                end = index - 1
+            else:
+                start = index + 1
+            if counter > 10:
+                break
+        return -1
