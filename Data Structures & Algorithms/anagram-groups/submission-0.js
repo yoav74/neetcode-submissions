22df752f1665011dee4b3379f343a2cf570c1333
Yoav Gershman <37370341+yoav74@users.noreply.github.com>
2026-05-01 17:52:49 +0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let ans = [];
+        let words = new Map();
+        for (let i = 0; i < strs.length ; i++){
+            let innerWord = strs[i].split('').sort().join();
+            if (!words.has(innerWord))
+                words.set(innerWord,[i]);
+            else{
+                words.get(innerWord).push(i);
+            }
+        }
+        for (let [key,val] of words){
+            let innerArr = [];
+            for (let idx of val){
+                innerArr.push(strs[idx]);
+            }
+            ans.push(innerArr);
+        }
+         return ans;
+    }
+}
