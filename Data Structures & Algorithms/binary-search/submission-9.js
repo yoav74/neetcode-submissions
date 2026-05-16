8d467fb5852ea3fd90617d13ef4377b0c50e2d35
Yoav Gershman <37370341+yoav74@users.noreply.github.com>
2026-05-16 18:39:36 +0300

    Add: binary-search - submission-9

---
+++ Data Structures & Algorithms/binary-search/submission-9.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let start = 0;
+        let end = nums.length - 1;
+        while (end >= start){
+            let mid = Math.floor((end + start) / 2);
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
